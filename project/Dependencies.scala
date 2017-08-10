0c0cf38afcbf48968ec3783712ef6b2a17f56086
Sunxiang <sunxiang@madhouse-inc.com>
2017-08-10 10:10:23 +0800

    avro schema增加location

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,19 +2,21 @@ import sbt._
 
 object Dependencies {
   // Versions
-  val sparkVersion  = "2.2.0"
-  val avroVersion   = "1.8.2"
+  val sparkVersion = "2.2.0"
+  val avroVersion  = "1.8.2"
 
   // Libraries
-  val config  = "com.typesafe"       % "config"                    % "1.3.1"
-  val sparkSM = "org.apache.spark"  %% "spark-streaming"           % sparkVersion   % Provided exclude("org.apache.hadoop", "hadoop-client") exclude("org.apache.mesos", "mesos")
-  val sparkSK = "org.apache.spark"  %% "spark-streaming-kafka-0-8" % sparkVersion
-  val hadoopC = "org.apache.hadoop"  % "hadoop-client"             % "2.7.3"        % Provided
-  val avro    = "org.apache.avro"    % "avro"                      % avroVersion
+  val config     = "com.typesafe"       % "config"                    % "1.3.1"
+  val hadoopC    = "org.apache.hadoop"  % "hadoop-client"             % "2.7.3"        % Provided
+  val sparkS     = "org.apache.spark"  %% "spark-streaming"           % sparkVersion   % Provided  exclude("org.apache.hadoop", "hadoop-client")
+  val sparkSK    = "org.apache.spark"  %% "spark-streaming-kafka-0-8" % sparkVersion
+  val avro       = "org.apache.avro"    % "avro"                      % avroVersion
+
 
   // Projects
-  val rootDeps = Seq(config, sparkSM, sparkSK, hadoopC, avro)
+  val rootDeps = Seq(config, hadoopC, sparkS, sparkSK, avro)
 
   // Resolvers
   val rootResolvers = Seq()
 }
+
